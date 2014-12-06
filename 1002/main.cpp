d725cb20961c835b89d522c937784b9f1d27997c
alex <peinlcy@sina.cn>
2014-12-06 19:43:51 +0800

    1002

---
+++ 1002/main.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+
+using namespace std;
+
+void Init(char **a,char **b,int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            b[i][j] = a[i][j];
+        }
+    }
+}
+
+void Calculate(char **b,int n,int *max)
+{
+    int tmp=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(b[i][j] == 'o')
+                tmp++;
+        }
+    }
+    if(tmp > *max)
+    {
+        *max = tmp;
+    }
+}
+
+void Choose(char **a,int i,int j,int n,int *max)
+{
+    char **b = new char*[n];
+    for(int i=0;i<n;i++)
+    {
+        b[i] = new char[n];
+    }
+    Init(a,b,n);
+    if(b[i][j] == '.')
+    {
+        b[i][j]='o';
+        for(int p=j+1;p<n;p++)
+        {
+            if(b[i][p]=='.')
+            {
+                b[i][p]='X';
+            }else
+            {
+                break;
+            }
+        }
+        for(int p=i+1;p<n;p++)
+        {
+            if(b[p][j]=='.')
+            {
+                b[p][j]='X';
+            }else
+            {
+                break;
+            }
+        }
+        for(int p=j-1;p>-1;p--)
+        {
+            if(b[i][p]=='.')
+            {
+                b[i][p]='X';
+            }else
+            {
+                break;
+            }
+        }
+        for(int p=i-1;p>-1;p--)
+        {
+            if(b[p][j]=='.')
+            {
+                b[p][j]='X';
+            }else
+            {
+                break;
+            }
+        }
+    }
+    if(i==n-1 && j==n-1)
+    {
+        Calculate(b,n,max);
+    }else if(j==n-1){
+        Choose(b,i+1,0,n,max);
+    }else{
+        Choose(b,i,j+1,n,max);
+    }
+    for(int i=0;i<j;i++)
+    {
+        delete[]b[i];
+    }
+    delete[]b;
+}
+
+int main()
+{
+    char **a;
+    int n,max=0;
+    while(1)
+    {
+        cin>>n;
+        if(n == 0)
+        {
+            break;
+        }
+        a = new char*[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i] = new char[n];
+            for(int j=0;j<n;j++)
+            {
+                cin>>a[i][j];
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                Choose(a,i,j,n,&max);
+            }
+        }
+        cout<<max<<endl;
+    }
+    for(int i=0;i<n;i++)
+    {
+        delete[]a[i];
+    }
+    delete[]a;
+    return 0;
+}
