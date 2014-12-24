a5b42cc6f58d6b6ac4cb357743ece0673639ff2d
lichangyuan <lichangyuan@baidu.com>
2014-12-24 16:11:26 +0800

    to be done

--- 1003/main.cpp
+++ 1003/main.cpp
@@ -3,107 +3,101 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-bool isOK(vector<int> v);
-int isupheld(vector<int> vhigh,vector<int> vlow);
-bool Factor(vector<int> vhigh,vector<int> vlow,int tmp = 1)
-{
 
-}
+bool numberIsUsed[101];
 
-bool canFactor(vector<int> large,int small = 100)
+bool CanFactor(int number,int factor = 100)
 {
-    if(isOK(large))
-        reture true;
-    vector<int>::iterator iter;
-    vector<int> vlarge(large);
-    for(iter = large.begin();iter != large.end();)
+    if(number == 1)
     {
-        if(*iter%small == 0 && find(large.begin(),large,end(),small)==large.end() && find(large.begin(),large,end(),*iter/small)==large.end() && *iter/small != small)
-        {
-            large.push_back(small);
-            large.push_back(*iter/small);
-            iter = large.erase(iter);
-            if(canFactor(large,small-1))
-                return true;
-            else
-                continue;
-        }else
-        {
-            iter++;
-        }
+        return true;
     }
-    return canFactor(vlarge,small-1);
-}
-
-bool isOK(vector<int> v)
-{
-    vector<int>::iterator vend = v.end();
-    for(vector<int>::iterator iter = v.begin(); iter!=vend; iter++)
-    {
-        if(*iter>100)
-            return false;
-    }
-    return true;
-}
-
-int isupheld(vector<int> vhigh,vector<int> vlow)
-{
-    if(isOK(vhigh) && isOK(vlow))
-    {
-        vector<int>::iterator vend = vhigh.end();
-        vector<int>::iterator findit;
-        int index=-1;
-        for(vector<int>::iterator iter = vhigh.begin(); iter!=vend; iter++)
-        {
-            index++;
-            findit = find(vlow.begin(),vlow.end(),*iter);
-            if(findit != vlow.end())
-                return index;
-        }
-    }
-    return -1;
-}
-
-bool Referee(vector<int> vhigh,vector<int> vlow,int small = 100)
-{
-
-}
-
-bool WinnerIsHigh(vector<int> vhigh,vector<int> vlow)
-{
-    if(!canFactor(vlow))
+    if(factor<2)
     {
         return false;
-    }else if(Referee(vhigh,vlow))
+    }
+    for(;factor>1;factor--)
+    {
+        if(number%factor == 0 && numberIsUsed[factor] == false)
+        {
+            break;
+        }
+    }
+    if(factor == 1)
+    {
+        return false;
+    }
+    numberIsUsed[factor] = true;
+    if(CanFactor(number/factor,factor-1))
+    {
+        return true;
+    }else
+    {
+        numberIsUsed[factor] = false;
+    }
+    return CanFactor(number,factor-1);
+}
+
+bool IsOK(int high,int low,int factor = 100)
+{
+    if(low == 1 && CanFactor(high))
+    {
+        return true;
+    }
+    if(factor == 1)
+    {
+        return false;
+    }
+    for(;factor>1;factor--)
+    {
+        if(low%factor == 0 && numberIsUsed[factor]==false)
+        {
+            break;
+        }
+    }
+    if(factor == 1)
+    {
+        return false;
+    }
+    numberIsUsed[factor] = true;
+    if(IsOK(high,low/factor,factor-1))
+    {
+        return true;
+    }
+    numberIsUsed[factor] = false;
+    return IsOK(high,low,factor-1);
+}
+
+bool IsHighWin(int high,int low,int factor = 100)
+{
+    for(int i=1;i<101;i++)
+    {
+        numberIsUsed[i] = false;
+    }
+    if(!CanFactor(low) || IsOK(high,low))
     {
         return true;
     }
     return false;
-
 }
 
 int main()
 {
     int high,low;
-    vector<int>vhigh,vlow;
-    while(cin>>high>>low)
+    cin>>high>>low;
+    //^（异或）用于交换
+    if(high<low)
     {
-        vhigh.reserve(100);
-        vlow.reserve(100);
-        if(high<low)
-        {
-            int tmp = high;
-            high = low;
-            low = tmp;
-        }
-        vhigh.push_back(high);
-        vlow.push_back(low);
-        if(WinnerIsHigh(vhigh,vlow))
-            cout<<high<<endl;
-        else
-            cout<<low<<endl;
-        vhigh.clear();
-        vlow.clear();
+        low = high ^ low;
+        high = high ^ low;
+        low = high ^ low;
+    }
+    if(IsHighWin(high,low))
+    {
+        cout<<high<<endl;
+    }else
+    {
+        cout<<low<<endl;
     }
     return 0;
 }
